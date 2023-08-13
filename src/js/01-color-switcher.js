959d69950eef0c73842607cbdb010ce4e06aea0c
Sergii-Drozdiuk <drozduyk@gmail.com>
2023-08-14 01:25:24 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+stopBtn.setAttribute("disabled", "")
+
+let timerId = null;
+
+startBtn.addEventListener("click", () => {
+  timerId = setInterval(() => {
+     document.body.style.backgroundColor = getRandomHexColor();
+     startBtn.setAttribute("disabled", "");
+     stopBtn.removeAttribute("disabled");
+  }, 1000);
+});
+
+
+stopBtn.addEventListener("click", () => {
+   clearInterval(timerId);
+   stopBtn.setAttribute("disabled", "");
+   startBtn.removeAttribute("disabled");
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
