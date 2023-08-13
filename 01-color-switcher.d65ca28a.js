390f3de7c1a8505501d34fd50477e8fec1cbc517
Sergii-Drozdiuk <126243923+Sergii-Drozdiuk@users.noreply.github.com>
2023-08-13 22:25:52 +0000

    Deploying to gh-pages from @ Sergii-Drozdiuk/goit-js-hw-09@959d69950eef0c73842607cbdb010ce4e06aea0c 🚀

---
+++ 01-color-switcher.d65ca28a.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");e.setAttribute("disabled","");let d=null;t.addEventListener("click",()=>{d=setInterval(()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`,t.setAttribute("disabled",""),e.removeAttribute("disabled")},1e3)}),e.addEventListener("click",()=>{clearInterval(d),e.setAttribute("disabled",""),t.removeAttribute("disabled")});
+//# sourceMappingURL=01-color-switcher.d65ca28a.js.map
