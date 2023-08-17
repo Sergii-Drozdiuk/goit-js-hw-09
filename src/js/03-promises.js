9194bb305f63bb69ac810c9a7dbfcce6d7a98223
Sergii-Drozdiuk <drozduyk@gmail.com>
2023-08-17 21:06:19 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,33 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const form = document.querySelector('.form');
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (Math.random() > 0.3) {
+            resolve({ position, delay });
+          } else {
+            reject({ position, delay });
+          }
+        }, delay);
+      });
+    }
+
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      
+      const delay = parseInt(event.target.delay.value);
+      const step = parseInt(event.target.step.value);
+      const amount = parseInt(event.target.amount.value);
+
+      for (let i = 1; i <= amount; i += 1) {
+        Promise.resolve().then(() => {
+          return createPromise(i, delay + (i - 1) * step)
+            .then((promise) => {
+              Notify.success(`✅ Fulfilled promise ${promise.position} in ${promise.delay}ms`);
+            })
+            .catch((error) => {
+              Notify.failure(`❌ Rejected promise ${error.position} in ${error.delay}ms`);
+            });
+        });
+      }
+    });
